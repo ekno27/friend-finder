c771a150ab95e61d409bb96d3849623d106bf407
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-21 23:29:46 -0700

    fixed image sizing and added more friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var people = [
     {
     "name":"Ahmed",
-    "photo":"http://pngimg.com/uploads/box/box_PNG60.png",
+    "photo":"https://www.designzzz.com/wp-content/uploads/2012/12/freestock_image_by_goldman555-d495w4s.jpg",
     "scores":[
         5,
         1,
@@ -17,21 +17,90 @@ var people = [
       ]
   },
   {
-    "name":"Lucas",
+    "name":"Joe",
     "photo":"http://pngimg.com/uploads/box/box_PNG60.png",
     "scores":[
         3,
         2,
-        4,
-        4,
-        4,
         1,
         5,
-        5,
+        3,
         2,
+        1,
+        1,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"The beast",
+    "photo":"https://assets3.thrillist.com/v1/image/1299823/size/tmg-article_default_mobile.jpg",
+    "scores":[
+        1,
+        4,
+        4,
+        3,
+        1,
+        4,
+        3,
+        2,
+        1,
+        1
+      ]
+  },
+
+  {
+    "name":"Jessica from accounting",
+    "photo":"https://i.pinimg.com/originals/bd/3a/78/bd3a7884e2dd373d2ae40200f8a2009b.png",
+    "scores":[
+        1,
+        4,
+        4,
+        3,
+        1,
+        4,
+        3,
+        2,
+        1,
+        1
+      ]
+  },
+  {
+    "name":"Jessica from accounting",
+    "photo":"https://i.pinimg.com/originals/bd/3a/78/bd3a7884e2dd373d2ae40200f8a2009b.png",
+    "scores":[
+        1,
+        4,
+        4,
+        3,
+        1,
+        4,
+        3,
+        2,
+        1,
+        1
+      ]
+  },
+  
+
+  {
+    "name":"Honextly don't even try",
+    "photo":"https://media.gettyimages.com/photos/portrait-of-happy-laughing-man-picture-id492529287?b=1&k=6&m=492529287&s=612x612&w=0&h=Cm8I6lOyf9xyEtYdF_PMUQy4HipiQ9IeAAxedwzw1W8=",
+    "scores":[
+        1,
+        4,
+        4,
+        3,
+        1,
+        4,
+        3,
+        2,
+        1,
         1
       ]
   }
+  
 ]
 
 module.exports = people;
